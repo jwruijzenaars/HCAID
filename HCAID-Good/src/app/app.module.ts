32efed82e04291df067995ddeeebe477c4bb06e4
jwruijzenaars <jw.ruijzenaars@student.avans.nl>
2024-10-28 16:31:37 +0100

    updated pages, added carousel to homepage and moved predictor to own page

--- HCAID-Good/src/app/app.module.ts
+++ HCAID-Good/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './auth/register/register.component';
+import { PredictorComponent } from './predictor/predictor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    PredictorComponent
   ],
   imports: [
     BrowserModule,
