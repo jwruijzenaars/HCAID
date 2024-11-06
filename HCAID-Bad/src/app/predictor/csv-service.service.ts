fa0560806b0c3f8b1d9fe89d1ab5ed85d18b7403
jwruijzenaars <jw.ruijzenaars@student.avans.nl>
2024-11-06 16:11:01 +0100

    Updated homepage and fixed 'bad' predictor

--- HCAID-Bad/src/app/predictor/csv-service.service.ts
+++ HCAID-Bad/src/app/predictor/csv-service.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Game } from './game';
 import { DropDownGame } from './dropdown-game';
+import { Genre } from './category copy';
+import { Category } from './category';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,86 @@ export class CsvService {
   private csvUrl = 'assets/steam.csv';
 
   constructor(private http: HttpClient) { }
+
+  getCategories(): Observable<Category[]> {
+    // read a csv file and return the list of categories
+    console.log('Reading the csv file');
+
+    return this.http.get(this.csvUrl, { responseType: 'text' }).pipe(
+      map(data => this.csvToCategoryArray(data))
+    );
+  }
+
+  csvToCategoryArray(data: string): any {
+    const lines = data.split('\n');
+    const result: Category[] = [];
+    const headers = lines[0].split(',');
+
+    console.log('Headers: ', headers);
+
+    for (let i = 1; i < lines.length; i++) {
+      const currentLine = lines[i].split(',');
+
+      if (currentLine.length !== headers.length) {
+        continue;
+      }
+
+      // split the categories by ;
+      const categories = currentLine[headers.indexOf('categories')].split(';');
+
+      // check if the category is already in the result
+      for (const category of categories) {
+        const newCategory: Category = {
+          category: category.trim()
+        };
+
+        if (!result.some((item) => item.category === newCategory.category)) {
+          result.push(newCategory);
+        }
+      }
+    }
+    return result;
+  }
+
+  getGenres(): Observable<Genre[]> {
+    // read a csv file and return the list of genres
+    console.log('Reading the csv file');
+
+    return this.http.get(this.csvUrl, { responseType: 'text' }).pipe(
+      map(data => this.csvToGenreArray(data))
+    );
+  }
+
+  csvToGenreArray(data: string): any {
+    const lines = data.split('\n');
+    const result: Genre[] = [];
+    const headers = lines[0].split(',');
+
+    console.log('Headers: ', headers);
+
+    for (let i = 1; i < lines.length; i++) {
+      const currentLine = lines[i].split(',');
+
+      if (currentLine.length !== headers.length) {
+        continue;
+      }
+
+      // split the genres by ;
+      const genres = currentLine[headers.indexOf('genres')].split(';');
+
+      // check if the genre is already in the result
+      for (const genre of genres) {
+        const newGenre: Genre = {
+          genre: genre.trim()
+        };
+
+        if (!result.some((item) => item.genre === newGenre.genre)) {
+          result.push(newGenre);
+        }
+      }
+    }
+    return result;
+  }
 
   getDropDownGames(): Observable<DropDownGame[]> {
     // read a csv file and return the list of games
